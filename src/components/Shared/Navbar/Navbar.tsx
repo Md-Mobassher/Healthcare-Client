8f1e08891f476531d30dad816c7d7a32995ccf3d
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-05-04 07:25:47 +0600

    navbar added

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,8 +1,44 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div>
-      <h1>This is Navbar component</h1>
-    </div>
+    <Container>
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography component="h1" variant="h4" fontWeight={600}>
+          P
+          <Box component="span" color="primary.main">
+            H
+          </Box>{" "}
+          Health Care
+        </Typography>
+        <Stack direction="row" justifyContent="space-between" gap={3}>
+          <Typography component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography component={Link} href="/healthplans">
+            Health Plans
+          </Typography>
+          <Typography component={Link} href="/medicine">
+            Medicine
+          </Typography>
+          <Typography component={Link} href="/diagonostics">
+            Diagonostics
+          </Typography>
+          <Typography component={Link} href="/ngos">
+            NGOs
+          </Typography>
+        </Stack>
+        <Button component={Link} href="/login">
+          Login
+        </Button>
+      </Stack>
+    </Container>
   );
 };
 
